85a9cd4d797d7f2bc98ccfc9cb12579b8b7b47aa
muskluckyy <muskluckyy@protonmail.com>
2025-02-17 12:37:16 +0000

    Adjusted header and pefected login and logout feature

--- react-workspace/todo-ui/src/App.jsx
+++ react-workspace/todo-ui/src/App.jsx
@@ -2,12 +2,25 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListTodoComponent from './components/ListTodoComponent'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import TodoComponent from './components/TodoComponent'
 import RegisterComponent from './components/RegisterComponent'
 import LoginComponent from './components/LoginComponent'
+import { isUserLoggedIn } from './services/AuthService'
 
 function App() {
+
+  function AuthenticatedRoute({ children }) {
+
+    const isAuth = isUserLoggedIn();
+
+    if (isAuth) {
+      return children;
+    }
+
+    return <Navigate to="/" />
+
+  }
 
   return (
     <>
@@ -15,17 +28,29 @@ function App() {
         <HeaderComponent />
         <Routes>
           {/* http://localhost:3000 */}
-          <Route path='/' element={<ListTodoComponent />}></Route>
+          <Route path='/' element={<LoginComponent />}></Route>
           {/* http://localhost:3000/todos */}
-          <Route path='/todos' element={<ListTodoComponent />}></Route>
+          <Route path='/todos' element={
+            <AuthenticatedRoute>
+              <ListTodoComponent />
+            </AuthenticatedRoute>}>
+          </Route>
           {/* http://localhost:3000/add-todo */}
-          <Route path='/add-todo' element={<TodoComponent />}></Route>
+          <Route path='/add-todo' element={
+            <AuthenticatedRoute>
+              <TodoComponent />
+            </AuthenticatedRoute>}>
+          </Route>
           {/* http://localhost:3000/update-todo/{id} */}
-          <Route path='/update-todo/:id' element={<TodoComponent />}></Route>
-{/* http://localhost:3000/register */}
-          <Route path='/register' element={ <RegisterComponent /> }></Route>
-        {/* http://localhost:3000/login */}
-        <Route path='/login' element={ <LoginComponent /> }></Route>
+          <Route path='/update-todo/:id' element={
+            <AuthenticatedRoute>
+              <TodoComponent />
+            </AuthenticatedRoute>}>
+          </Route>
+          {/* http://localhost:3000/register */}
+          <Route path='/register' element={<RegisterComponent />}></Route>
+          {/* http://localhost:3000/login */}
+          <Route path='/login' element={<LoginComponent />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
--- react-workspace/todo-ui/src/components/HeaderComponent.jsx
+++ react-workspace/todo-ui/src/components/HeaderComponent.jsx
@@ -1,35 +1,62 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { isUserLoggedIn, logout } from '../services/AuthService'
 
 const HeaderComponent = () => {
-  return (
-    <div>
-        <header>
-            <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
-                <div>
-                    <a href='http://localhost:3000' className='navbar-brand'>
-                        Todo Management Application
-                    </a>
-                </div>
-                <div className='collapse navbar-collapse'>
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <NavLink to="/todos" className="nav-link">Todos</NavLink>
-                        </li>
-                    </ul>
-                </div>
-                <div className='navbar-nav'>
-                        <li className='nav-item'>
-                            <NavLink to="/register" className="nav-link">Register</NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink to="/login" className="nav-link">Login</NavLink>
-                        </li>
-                </div>
-            </nav>
-        </header>
-    </div>
-  )
+    const isAuth = isUserLoggedIn();
+
+    const navigator = useNavigate()
+
+    function handleLogout(){
+        logout();
+        navigator('/login')
+    }
+
+    return (
+        <div>
+            <header>
+                <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+                    <div>
+                        <a href='http://localhost:3000' className='navbar-brand'>
+                            Todo Management Application
+                        </a>
+                    </div>
+                    <div className='collapse navbar-collapse'>
+                        <ul className='navbar-nav'>
+                            {
+                                isAuth &&
+                                <li className='nav-item'>
+                                    <NavLink to="/todos" className="nav-link">Todos</NavLink>
+                                </li>
+                            }
+
+                        </ul>
+                    </div>
+                    <div className='navbar-nav'>
+                        {
+                            !isAuth &&
+                            <li className='nav-item'>
+                                <NavLink to="/register" className="nav-link">Register</NavLink>
+                            </li>
+                        }
+                        {
+                            !isAuth &&
+                            <li className='nav-item'>
+                                <NavLink to="/login" className="nav-link">Login</NavLink>
+                            </li>
+                        }
+                        {
+                            isAuth &&
+                            <li className='nav-item'>
+                                <NavLink to="/login" className="nav-link" onClick={handleLogout}>Logout</NavLink>
+                            </li>
+                        }
+
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default HeaderComponent
